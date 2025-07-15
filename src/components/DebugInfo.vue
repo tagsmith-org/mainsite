1d748c1eb2a5bb1445acc96d4e85f7e64520906b
Serg Kostyuk <serg.kostyuk@gmail.com>
2025-07-15 13:06:07 -0400

    Fourth

--- src/components/DebugInfo.vue
+++ src/components/DebugInfo.vue
@@ -23,7 +23,7 @@ const updateWidth = () => {
         mainWidth.value = mainElement.offsetWidth
     }
 }
-// ddfg 777777 66666bbbb
+// ddfg 777777 66666bbbb врапрапр
 const updatePage = () => {
     currentPage.value = route.name as string || route.path.slice(1) || 'home'
 }
